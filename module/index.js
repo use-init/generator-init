82612507a14857a710130c6086a473ecef505b6e
Kahlil Lechelt <hello@kahlil.co>
2013-12-18 13:35:32 +0100

    Add test-spec in test/spec.js.

--- module/index.js
+++ module/index.js
@@ -1,5 +1,7 @@
 'use strict';
 var util   = require('util');
+var path   = require('path');
+var fs     = require('fs');
 var yeoman = require('yeoman-generator');
 
 var ModuleGenerator = module.exports = function ModuleGenerator() {
@@ -23,4 +25,23 @@ ModuleGenerator.prototype.module = function module() {
 
 ModuleGenerator.prototype.test = function test() {
   this.template('example.spec.js', 'test/specs/' + this.name + '.spec.js');
+};
+
+ModuleGenerator.prototype.loadSpecs = function loadSpecs() {
+  var specsToLoad, specFileContent, specTpl;
+  var specs       = [];
+  var specsPath   = path.join(process.cwd(), 'test/specs');
+
+  if (fs.existsSync(specsPath)) {
+    specs = fs.readdirSync(specsPath);
+    specs = specs.map(function cutFileending(specFile) {
+      return specFile.replace('.js', '');
+    });
+    specsToLoad     = '"' + specs.join('", "') + '"';
+    specTpl         = path.join(this.sourceRoot(), 'spec.js');
+    specFileContent = this.readFileAsString(specTpl);
+    specFileContent = specFileContent.replace('<%= specs %>', specsToLoad);
+
+    this.write(path.join(process.cwd(), 'test/spec.js'), specFileContent);
+  }
 };
